24ce6af1d668d03db53ed5d8210fe90a47c562eb
Renato Cruzinha <puyol@Renatos-MBP.lan>
2020-06-05 22:52:25 +0100

    add dados

---
+++ readcsv.py
@@ -0,0 +1,22 @@
+import csv
+import sys
+
+orig_stdout = sys.stdout
+f = open('out.pl', 'w')
+sys.stdout = f
+
+with open('paragens.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+        else:
+            if (row[9].find('\'') != -1): 
+                row[9].replace('\'', ' ')
+            print(f'paragem({row[0]}, {row[1]}, {row[2]}, \'{row[3]}\', \'{row[4]}\', \'{row[5]}\', \'{row[6]}\', [{row[7]}], {row[8]}, \'{row[9]}\', [\'{row[10]}\']).')
+            line_count += 1
+
+
+sys.stdout = orig_stdout
+f.close()
